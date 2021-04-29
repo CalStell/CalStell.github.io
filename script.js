aca4c70a26c191978985c628017ae985605fbe75
Cal Stellato <cal_stellato@brown.edu>
2021-04-29 00:32:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const modal = document.querySelector('.modal'); //we're storing this in a constant called modal
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal); //querySelectorAll returns a node list of all the elements in the show-modal class (in the html)
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(
+    'click',
+    openModal
+    //we want to display modal now (when a button is clicked)
+    //NO DOT here. Just hidden. This removes the hidden from the modal class
+  );
+}
+
+function closeModal() {
+  modal.classList.add('hidden'); //hiding the modal when you click on the X
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+  if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+    closeModal();
+});
